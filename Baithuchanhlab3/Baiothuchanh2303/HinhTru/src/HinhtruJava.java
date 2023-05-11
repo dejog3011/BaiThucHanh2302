ab4b468778a976a395c4b3254ab0478b99033363
Quang Minh <89411542+dejog3011@users.noreply.github.com>
2023-05-11 13:15:17 +0700

    Add files via upload

---
+++ Baithuchanhlab3/Baiothuchanh2303/HinhTru/src/HinhtruJava.java
@@ -0,0 +1,12 @@
+public class HinhtruJava {
+
+    public static void main(String[] args) {
+        hinhtru ht = new hinhtru();
+        ht.nhapThongTin();
+        ht.tinhTheTich();
+        ht.tinhDienTichXungQuanh();
+        ht.tinhDienTichToanPhan();
+        ht.hienThi();
+    }
+
+}
---
+++ Baithuchanhlab3/Baiothuchanh2303/Nhanvien/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        nhanvien nv = new nhanvien();
+        nv.nhapDuLieu();
+        nv.tinhLuong();
+        nv.inDuLieu();
+    }
+}
